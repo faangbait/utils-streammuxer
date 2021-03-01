0002d3313863fcc3f8c2c8fcba1a87fa314f712a
Sage Smith <sage@limitlessinteractive.com>
2021-02-28 23:29:42 -0600

    cleanup

--- generate_clean_audiovisual.sh
+++ generate_clean_audiovisual.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#ffmpeg -re -i rtmp://localhost:1935/final_audio -i rtmp://localhost:1935/final_video -c copy -f flv pipe:
-
-
-
 ffmpeg -f flv -i pipe: \
 -vcodec libx264 -g 60 -keyint_min 30 -b:v 3000k -minrate 3000k -maxrate 3000k \
 -vf "scale=1280:-1,format=yuv420p" -preset veryfast \
--- raw_video.sh
+++ raw_video.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-#ffmpeg -f x11grab -s 1920x1080 -framerate 30 -i :0.0 -f pulse -ac 2 -i 3 -c:v libx264 -preset veryfast -c:a aac -f flv rtmp://localhost:1935/raw_video_audio
-#ffmpeg -f x11grab -s 1920x1080 -framerate 30 -i :0.0 -f pulse -ac 2 -i 3 -c:v libx264 -preset veryfast -c:a aac -f avi test.avi
-
-#ffmpeg -f x11grab -s 1920x1080 -framerate 30 -i :0.0 -c copy rtmp://localhost:1935/final_video -f pulse -ac 2 -i 3 rtmp://localhost:1935/final_audio
 ffmpeg -video_size 1920x1080 -f x11grab -framerate 30 -i :0.0 -f pulse -ac 2 -i 3 -c:v libx264rgb -crf 0 -preset ultrafast -f flv pipe:
